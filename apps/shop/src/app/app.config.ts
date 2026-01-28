5bf61b8e4cecb5de5a6a633e4c8b88c26f139809
Ahmed Zienhom <ahmedalnafy@gmail.com>
2026-01-29 01:01:50 +0200

    adding needed packages (tailwindcss-primen v21 - lucid)

--- apps/shop/src/app/app.config.ts
+++ apps/shop/src/app/app.config.ts
@@ -1,10 +1,28 @@
 import {
   ApplicationConfig,
+  importProvidersFrom,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/aura';
+import { AlarmClock, BadgeCheck, LucideAngularModule} from 'lucide-angular';
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [provideBrowserGlobalErrorListeners(),
+    importProvidersFrom(
+      LucideAngularModule.pick({
+        AlarmClock,
+        BadgeCheck
+      })
+    ), 
+    provideRouter(appRoutes),
+    providePrimeNG({
+    theme: {
+      preset: Lara
+    }
+  })],
 };
