2e997a5067010fd751f7cc397ded32999caa94cb
Ahmed Elsayed <ahmedzekeba6@gmail.com>
2026-02-07 19:52:38 +0200

    feat: create AuthLayout template component

---
+++ libs/shared/ui/src/lib/components/templates/auth-layout/auth-layout.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'lib-auth-layout',
+  imports: [],
+  templateUrl: './auth-layout.html',
+  styleUrl: './auth-layout.css',
+})
+export class AuthLayout {
+     bannerImage = input.required<string>();
+  title = input<string>();
+  subtitle = input<string>();
+}
