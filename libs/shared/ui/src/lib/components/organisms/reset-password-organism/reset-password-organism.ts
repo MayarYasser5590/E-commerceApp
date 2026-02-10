08d322bb6fcef7f1b6fd9e1c44c70e605010aae5
MayarYasser5590 <MayarYasser5590@gmail.com>
2026-02-10 20:20:04 +0200

    initial

---
+++ libs/shared/ui/src/lib/components/organisms/reset-password-organism/reset-password-organism.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-reset-password-organism',
+  imports: [],
+  templateUrl: './reset-password-organism.html',
+  styleUrl: './reset-password-organism.scss',
+})
+export class ResetPasswordOrganism {}
---
+++ libs/shop/feature-home/src/lib/reset-password-feature/reset-password-feature.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-reset-password-feature',
+  imports: [],
+  templateUrl: './reset-password-feature.html',
+  styleUrl: './reset-password-feature.scss',
+})
+export class ResetPasswordFeature {}
