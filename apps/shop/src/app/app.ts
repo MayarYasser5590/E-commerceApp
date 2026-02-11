3d8b42eed7b7533cc80d2d09a196a9ef6f71695f
Basem-Serag <basemserag.911@gmail.com>
2026-02-11 16:47:06 +0000

    feat: enable shop feature home component

--- apps/shop/src/app/app.ts
+++ apps/shop/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-// import { ShopFeatureHome } from "@shop-workspace/shop-feature-home";
+import { ShopFeatureHome } from '@shop-workspace/shop-feature-home';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, ShopFeatureHome],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
