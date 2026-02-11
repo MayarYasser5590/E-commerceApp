ceed73b43e14be962b018a4f5670335cca0747f9
Basem-Serag <basemserag.911@gmail.com>
2026-02-11 16:47:07 +0000

    feat: export new components from shared UI library

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -4,8 +4,12 @@
 export * from './lib/components/atoms/lib-button/lib-button';
 export * from './lib/components/atoms/toast/toast';
 export * from './lib/components/atoms/label/label';
+export * from './lib/components/atoms/custom-input/custom-input';
+export * from './lib/components/atoms/select-input/select-input';
 // Molecules
 export * from './lib/components/molecules/AuthFormHeaderMolecule/AuthFormHeaderMolecule'
+export * from './lib/components/molecules/form-field/form-field';
+export * from './lib/components/molecules/phone-input/phone-input';
 // Organisms
 export * from './lib/components/organisms/verify-otp-form-organism/verify-otp-form-oragnism'
 // Templates
