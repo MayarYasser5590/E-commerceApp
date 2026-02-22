7554a6abc9e0767f21ae8daf5bb92c385c5ef116
Basem-Serag <basemserag.911@gmail.com>
2026-02-22 16:50:16 +0000

    Centralizes API endpoints in a single file.

--- libs/shared/util/src/index.ts
+++ libs/shared/util/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/util';
 export * from './lib/config.tokens';
 
-export * from './lib/validators/confirm-password-validator'
-export * from './lib/confirm-pass-error.utils'
+export * from './lib/validators/confirm-password-validator';
+export * from './lib/confirm-pass-error.utils';
+export * from './lib/constants/endpoints.const';
