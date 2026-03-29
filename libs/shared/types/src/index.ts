c506b1de5e82c038eddc9ceb446e11f1851a1422
Basem-Serag <basemserag.911@gmail.com>
2026-03-29 21:53:20 +0100

    Updates export to use revised product data interface
    Aligns exports with updated interface naming for improved clarity
    and consistency across shared types.

--- libs/shared/types/src/index.ts
+++ libs/shared/types/src/index.ts
@@ -1,7 +1,7 @@
 export * from './lib/types';
 
 
-export * from './lib/interfaces/product.interface';
+export * from './lib/interfaces/productData.interface';
 export * from './lib/interfaces/category.interface';
 export * from './lib/interfaces/occasion.interface';
 export * from './lib/interfaces/home.interface';
