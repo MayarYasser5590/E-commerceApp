df952f56d09c30eae2af8f012835498a2f972395
MayarYasser5590 <MayarYasser5590@gmail.com>
2026-04-01 01:05:31 +0200

    dark mode

--- libs/shared/ui/src/lib/components/organisms/top-navbar-organism/top-navbar-organism.ts
+++ libs/shared/ui/src/lib/components/organisms/top-navbar-organism/top-navbar-organism.ts
@@ -1,25 +1,50 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { LucideAngularModule , ChevronDown , User , MapPinPen   } from 'lucide-angular';
-import { SearchInputMolecule } from "../../molecules/search-input-molecule/search-input-molecule";
-import { LogoAtom } from "../../atoms/logo-atom/logo-atom";
-import { NavUserDataMolecule } from "../../molecules/nav-user-data-molecule/nav-user-data-molecule";
-import { AuthenticatedMenuMolecule } from "../../molecules/authenticated-menu-molecule/authenticated-menu-molecule";
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import {
+  LucideAngularModule,
+  ChevronDown,
+  User,
+  MapPinPen,
+  Moon,
+  SunMoon,
+} from 'lucide-angular';
+import { SearchInputMolecule } from '../../molecules/search-input-molecule/search-input-molecule';
+import { LogoAtom } from '../../atoms/logo-atom/logo-atom';
+import { NavUserDataMolecule } from '../../molecules/nav-user-data-molecule/nav-user-data-molecule';
+import { AuthenticatedMenuMolecule } from '../../molecules/authenticated-menu-molecule/authenticated-menu-molecule';
+import { ThemeService } from '@shop-workspace/shared-util';
 
 @Component({
   selector: 'lib-top-navbar-organism',
-  imports: [LucideAngularModule, SearchInputMolecule, LogoAtom, NavUserDataMolecule, AuthenticatedMenuMolecule],
+  imports: [
+    LucideAngularModule,
+    SearchInputMolecule,
+    LogoAtom,
+    NavUserDataMolecule,
+    AuthenticatedMenuMolecule,
+  ],
   templateUrl: './top-navbar-organism.html',
   styleUrl: './navbar-organism.scss',
   standalone: true,
 })
 export class TopNavbarOrganism {
-  @Input() isAuthenticated= false;
+  @Input() isAuthenticated = false;
   @Input() user: any;
   @Output() logout = new EventEmitter<void>();
+  private themeService = inject(ThemeService);
 
+  icons = {
+    ChevronDown,
+    User,
+    MapPinPen,
+    Moon,
+    SunMoon,
+  };
 
-    icons = {
-      ChevronDown , User , MapPinPen 
-    };
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
 
+  isDark() {
+    return this.themeService.theme() === 'dark';
+  }
 }
