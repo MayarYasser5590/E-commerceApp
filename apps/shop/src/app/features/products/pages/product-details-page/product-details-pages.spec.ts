7a2f5ef378d458828b23262131ba976bf3263248
Basem-Serag <basemserag.911@gmail.com>
2026-04-23 10:40:00 +0100

    test: provide app config in shop specs

--- apps/shop/src/app/features/products/pages/product-details-page/product-details-pages.spec.ts
+++ apps/shop/src/app/features/products/pages/product-details-page/product-details-pages.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductDetailsPages } from './product-details-pages';
+import { appConfig } from '../../../../app.config';
 
 describe('ProductDetailsPages', () => {
   let component: ProductDetailsPages;
@@ -8,6 +9,7 @@ describe('ProductDetailsPages', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProductDetailsPages],
+      providers: appConfig.providers,
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductDetailsPages);
